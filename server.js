eb8f444edfb44667e0ae525c27113bb91580c4b2
FabioDiCeglie <fabiod@backbase.com>
2023-10-11 10:22:04 +0200

    feat: create a sanity check

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
-const app = express();
-app.use(express.static(__dirname+'/public'));
-const expressServer = app.listen(9000);
 const socketio = require('socket.io');
+
+const app = express();
+
+app.use(express.static(__dirname+'/public'));
+
+const expressServer = app.listen(9000);
+const io = socketio(expressServer);
+
+
+console.info('Starting server: http://localhost:9000')
