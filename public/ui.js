d2fbd912bc55ca9f0609bf78dee488948cfcdcf7
FabioDiCeglie <fabiod@backbase.com>
2023-10-11 13:36:09 +0200

    feat: start working on the canvas

--- public/ui.js
+++ public/ui.js
@@ -1,1 +1,13 @@
-console.log("Sanity check")
+let wHeight = window.innerHeight;
+let wWidth = window.innerWidth;
+
+const canvas = document.querySelector('#the-canvas');
+const context = canvas.getContext('2d')
+
+canvas.height = wHeight;
+canvas.width = wWidth;
+
+window.addEventListener('load', () => {
+    const loginModal = new bootstrap.Modal(document.querySelector('#loginModal'));
+    loginModal.show()
+})
