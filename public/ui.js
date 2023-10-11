aac6e22c801dc55ca21e7bfb1f53fa0aaa0ecc3c
FabioDiCeglie <fabiod@backbase.com>
2023-10-11 13:47:37 +0200

    chore: DOM setup

--- public/ui.js
+++ public/ui.js
@@ -1,13 +1,26 @@
 let wHeight = window.innerHeight;
 let wWidth = window.innerWidth;
 
+//canvas element needs to be in a variable
 const canvas = document.querySelector('#the-canvas');
+//context is how we draw! We will be drawing in 2d
 const context = canvas.getContext('2d')
-
+//set the canvas height and width to = window height and width
 canvas.height = wHeight;
 canvas.width = wWidth;
+const player = {};
+
+const loginModal = new bootstrap.Modal(document.querySelector('#loginModal'));
+const spawnModal = new bootstrap.Modal(document.querySelector('#spawnModal'));
+
 
 window.addEventListener('load', () => {
-    const loginModal = new bootstrap.Modal(document.querySelector('#loginModal'));
     loginModal.show()
+});
+
+document.querySelector('.name-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    player.name = document.querySelector('#name-input').value;
+    loginModal.hide()
+    spawnModal.show()
 })
